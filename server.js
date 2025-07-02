7f6d1a7c5f6e376f3bc2d7a2e44fdfb617ec1bdb
Enivid345 <enividtheking@gmail.com>
2025-07-02 02:32:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -98,7 +98,9 @@ app.post("/dl", async (req, res) => {
     res.json({message: "error"})
   }
 });
-
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 app.listen(port, () => {
   console.log("Server is running on port 3000");
 })
